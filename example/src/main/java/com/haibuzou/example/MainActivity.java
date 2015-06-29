8d88aeb2922384fad81fb9ba12bd64333550b966
stone <542464069@qq.com>
2015-06-29 15:05:55 +0800

    示例修改

--- example/src/main/java/com/haibuzou/example/MainActivity.java
+++ example/src/main/java/com/haibuzou/example/MainActivity.java
@@ -1,9 +1,17 @@
 package com.haibuzou.example;
 
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import com.haibuzou.library.PullToRefreshListView;
+import com.haibuzou.library.PullToRefreshBase;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,27 +20,48 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        final Handler mhandler = new Handler();
+        final PullToRefreshListView pullToRefreshListView = (PullToRefreshListView)findViewById(R.id.pull_to_refresh);
+        final List<String> data = new ArrayList<>();
+        final Random random = new Random();
+        for(int i = 0; i < 10; i++){
+            data.add(random.nextInt(10)+"");
         }
+        final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,data);
+        pullToRefreshListView.setAdapter(adapter);
 
-        return super.onOptionsItemSelected(item);
+        pullToRefreshListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<ListView>() {
+            @Override
+            public void onPullDownToRefresh(PullToRefreshBase<ListView> refreshView) {
+                mhandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        pullToRefreshListView.onRefreshComplete();
+                    }
+                }, 2000);
+            }
+
+            @Override
+            public void onPullUpToRefresh(PullToRefreshBase<ListView> refreshView) {
+
+            }
+        });
+
+        pullToRefreshListView.setOnLastItemVisibleListener(new PullToRefreshBase.OnLastItemVisibleListener() {
+            @Override
+            public void onLastItemVisible() {
+                mhandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (int i = 0; i < 10; i++) {
+                            data.add("Add item" + random.nextInt(10));
+                        }
+                        adapter.notifyDataSetChanged();
+                        pullToRefreshListView.onRefreshComplete();
+                    }
+                }, 2000);
+            }
+        });
     }
+
 }
